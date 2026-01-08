8e5b430ba713ed04558d9dd990f0a16686f1c695
mandacx <54549842+mandacx@users.noreply.github.com>
2026-01-07 18:08:25 -0600

    Remove unsupported axisLabelBackgroundColor property

--- components/charts/KLineChart.tsx
+++ components/charts/KLineChart.tsx
@@ -381,7 +381,6 @@ export default function KLineChart({
           lineWidth: lineWidth,
           lineStyle: isClosest ? 0 : 2, // 0 = solid, 2 = dashed
           axisLabelVisible: true,
-          axisLabelBackgroundColor: color,
           title: displayName,
         });
 
